4ad738aed39fcce203f9823b9a3d32f0f933199f
kembreyfarquhar <kembreyfarquhar@gmail.com>
2022-01-03 20:55:51 -0600

    changes made in class

---
+++ models/language.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const languageSchema = new Schema({
+  name: { type: String, required: true },
+  greeting: String,
+  pangram: String,
+  filler: String,
+});
+
+const Language = mongoose.model("Language", languageSchema);
+module.exports = Language;
